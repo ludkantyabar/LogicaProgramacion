a7c32f73e70008879dea141c5cf5bb32e991691c
ludkant <ludkant@gmail.com>
2025-01-14 22:15:20 -0500

    ajercicio 5

--- src/ejer05.java
+++ src/ejer05.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class ejer05 {
     public static void main(String[] args) {
       /*Construir un programa que pida un año y luego determine si el año es bisiesto o no.
       Tenga en cuenta que los años bisiestos son los números múltiplos de 4,
       pero que no son múltiplos de 100 y si son múltiplos de 100 y múltiplos de 400 sí son bisiestos.*/
+
+        Scanner teclado = new Scanner(System.in);
+        int anio;
+        System.out.println("Ingrese un año: ");
+        anio = teclado.nextInt();
+        if(anio % 4 == 0 && anio % 100 != 0 || anio % 400 == 0){
+            System.out.println("El año " + anio + " es bisiesto");
+        }else{
+            System.out.println(anio + " no es bisiesto");
+        }
 
     }
 }
